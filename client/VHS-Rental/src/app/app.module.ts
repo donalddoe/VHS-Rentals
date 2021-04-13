2075c379cd9dd2fc30b05f6c402e6cc03b0f40bc
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2021-04-13 18:58:04 +0000

    grouped angular material into one component

--- client/VHS-Rental/src/app/app.module.ts
+++ client/VHS-Rental/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './screens/login/login.component';
+import { RegisterComponent } from './screens/register/register.component';
+import { MoviesComponent } from './screens/movies/movies.component';
+import { AdminComponent } from './screens/admin/admin.component';
+import { RentalsComponent } from './screens/rentals/rentals.component';
+import { ReturnsComponent } from './screens/returns/returns.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MoviesComponent,
+    AdminComponent,
+    RentalsComponent,
+    ReturnsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
