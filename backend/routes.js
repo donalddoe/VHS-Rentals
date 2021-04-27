51073c6e073c2f0ca96746e4b622cc1100d89e9a
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2021-04-27 08:51:35 +0000

    working on deploying frontend to heroku

--- backend/routes.js
+++ backend/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const users = require('../routes/users');
-const auth = require('../routes/auth');
-const movies = require("../routes/movies");
-const genres = require('../routes/genres');
-const customers = require('../routes/customers');
-const rentals = require('../routes/rentals');
-const returns = require('../routes/returns')
-const error = require('../middlewares/error');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const movies = require("./routes/movies");
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const rentals = require('./routes/rentals');
+const returns = require('./routes/returns')
+const error = require('./middlewares/error');
 const path = require('path');
 
 
@@ -15,10 +15,10 @@ module.exports = function(app) {
     app.use(express.urlencoded({extended : false}));
     app.use(cors());
     app.use(express.json());
-    const fileDirectory = (path.join(__dirname, "../Client/dist/VHS-Rental"))
+    const fileDirectory = (path.join(__dirname, "Client/dist/VHS-Rental"))
     app.use(express.static(fileDirectory));
     app.get("**", (req, res) => {
-      res.sendFile(path.join(__dirname, "../Client/dist/VHS-Rental/index.html"));
+      res.sendFile(path.join(__dirname, "Client/dist/VHS-Rental/index.html"));
       // res.sendFile("index.html", { root: fileDirectory }, (err) => {
       //   res.end();
     
