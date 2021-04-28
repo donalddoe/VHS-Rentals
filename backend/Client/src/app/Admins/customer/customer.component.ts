c59fa9a2ecb58df420e3703554354e93109b7f36
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2021-04-28 11:37:55 +0000

    Admin adding new user implemented

--- backend/Client/src/app/Admins/customer/customer.component.ts
+++ backend/Client/src/app/Admins/customer/customer.component.ts
@@ -3,6 +3,7 @@ import { LoaderService } from 'src/app/loader/loader.service';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { CustomerService } from 'src/app/customer.service';
 import Swal from 'sweetalert2';
+import { RegisterService } from 'src/app/register.service';
 
 
 @Component({
@@ -16,33 +17,56 @@ export class CustomerComponent implements OnInit {
   ngOnInit(): void {
   }
   constructor(
-    private customerService: CustomerService,
+    private service:RegisterService,
     public loaderService: LoaderService,
     public fb: FormBuilder,
     ) { 
       this.form = this.fb.group({
-        "name": ['',[Validators.required, Validators.minLength(5)]],
-        "phone": ['', [Validators.required, Validators.minLength(10), Validators.pattern("^[0-9]*$")]],
+        "username": ['',[Validators.required, Validators.minLength(5)]],
+        "email": new FormControl(null,[Validators.required, this.emailChecked]),
+      "password": new FormControl(null,[Validators.required, Validators.minLength(8),
+       Validators.pattern(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]))/g)]),
       })
     }
 
-    get phone(){
-      return this.form.get('phone');
+    
+
+
+
+
+emailChecked(control){
+  if(control.value !=null){
+    var regexp = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+    if(regexp.test(control.value) !==true){
+      return{
+        emailValidity:true
+      } 
     }
-    get name(){
-      return this.form.get('name')
+  }
+}
+
+get password() {
+  return this.form.get('password')
+}
+
+get email() {
+  return this.form.get('email')
+}
+
+    get username(){
+      return this.form.get('username')
     }
  
 
   onSubmit(form){
     console.log(form)
-    this.customerService.addCustomer(this.form.value).subscribe(
+    this.service.register(this.form.value).subscribe(
       response => {
         console.log(response)
         Swal.fire({
           position: 'center',
           icon: 'success',
-          title: 'Customer has been created',
+          title: 'User has been created',
           showConfirmButton: false,
           timer: 4000
         });
