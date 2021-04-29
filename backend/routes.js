2a90fcadd0490a5dcc4149be37b8ec7368dc47c9
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2021-04-29 15:12:49 +0000

    rentals routes modified

--- backend/routes.js
+++ backend/routes.js
@@ -15,16 +15,7 @@ module.exports = function(app) {
     app.use(express.urlencoded({extended : false}));
     app.use(cors());
     app.use(express.json());
-    const fileDirectory = (path.join(__dirname, "public/VHS-Rental"))
-    app.use(express.static(fileDirectory));
-    app.get("**", (req, res) => {
-      res.sendFile(path.join(__dirname, "public/VHS-Rental/index.html"));
-      // res.sendFile("index.html", { root: fileDirectory }, (err) => {
-      //   res.end();
-    
-      //   if (err) throw err;
-      // });  
-     });
+   
     app.use('/api/users', users);
     app.use('/api/auth', auth);
     app.use('/api/movies', movies);
@@ -32,5 +23,10 @@ module.exports = function(app) {
     app.use('/api/customers', customers);
     app.use('/api/rentals', rentals);
     app.use('/api/returns', returns);
+    const fileDirectory = (path.join(__dirname, "public/VHS-Rental"))
+    app.use(express.static(fileDirectory));
+    app.get("**", (req, res) => {
+      res.sendFile(path.join(__dirname, "public/VHS-Rental/index.html")); 
+     });
     app.use(error)
     }
