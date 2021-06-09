619ae3ac77eb4d42e43065dc81e250021670c731
Jocelyn <jobalangtaa@gmail.com>
2021-06-09 15:31:01 +0000

    added getWallet method

--- backend/Client/src/app/nav-bar/nav-bar.component.ts
+++ backend/Client/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { NavigationEnd, Router } from '@angular/router';
@@ -10,30 +10,38 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-wallet = null; 
+  wallet = null;
   constructor(
     // private auth: AuthService, 
     private router: Router
   ) {
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
-        const userCredential = localStorage.getItem('wallet');
-        if(userCredential) {
-          const wallet = userCredential;
-          this.wallet = wallet;
-        }
+        // const userCredential = localStorage.getItem('wallet');
+        // if(userCredential) {
+        //   const wallet = userCredential;
+        //   this.wallet = wallet;
+        // }
       }
     })
-   }
-  
+  }
+
+  getWallet() {
+    if (localStorage.getItem("wallet")) {
+      return localStorage.getItem("wallet")
+    }
+    return 0
+  }
+
   getNumberOfItemsInCart() {
-    let cart=[]
-    if(localStorage.getItem("cart")){
+    let cart = []
+    if (localStorage.getItem("cart")) {
       cart = JSON.parse(localStorage.getItem("cart"))
 
     }
     return cart.length
   }
+
 
   ngOnInit(): void {
   }
